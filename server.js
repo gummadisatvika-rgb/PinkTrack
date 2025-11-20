efbedce56c5c4dc5feaabdab55bf46f66945eaf7
gummadisatvika-rgb <gummadisatvika@gmail.com>
2025-11-20 20:38:35 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 const cors = require("cors");
 const app = express();
 const PORT = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+
 
 app.use(cors());
 app.use(express.json());
